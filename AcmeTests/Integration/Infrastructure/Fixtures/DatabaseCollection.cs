200eb32f9fc05084d75719d2d6677b31ee29b1dd
dfitzgibbons <david.fitzgibbons@oracle.com>
2019-06-23 20:43:54 -0400

    re-organized infrastruce tests

--- AcmeTests/Integration/Infrastructure/Fixtures/DatabaseCollection.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace AcmeTests.Integration.Fixtures
-{
-    using Xunit;
-
-    [CollectionDefinition("Database")]
-    public class DatabaseCollection : ICollectionFixture<DatabaseFixture>
-    {
-    }
-}
